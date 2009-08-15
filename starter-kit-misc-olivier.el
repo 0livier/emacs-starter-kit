d6f3a2bccee3d0b72f4a79fd3e0d86fc2bf9d28f
Olivier Garcia <olivier@linux-nerd.com>
2009-08-15 23:49:38 +0200

    Minor tweaks on yanking and pasting

--- starter-kit-misc-olivier.el
+++ starter-kit-misc-olivier.el
@@ -29,48 +29,10 @@ file modes."
 (add-hook 'write-file-hooks 'time-stamp)
 (setq time-stamp-active t)
 
-;; (add-hook 'php-mode-hook
-;;           '(lambda () ((setq indent-tabs-mode t)))
-;;           '(lambda () ((setq setq-default indent-tabs-mode t)))
-;;           '(lambda () ((local-set-key (kbd "TAB") 'self-insert-command))))
+(setq line-number-mode t)
+(setq column-number-mode t)
 
-;; ;; Set the tab width
-;; (setq default-tab-width 8)
-;; (setq tab-width 8)
-;; (setq c-basic-indent 8)
-
-
-; ---------------------- 
-; IS IT DONE BY STARTER KIT ?
-
-;; ;
-;; ; .spell
-;; ;
-;; (setq-default ispell-program-name "aspell")
-;; (set-language-environment 'latin-1)
-;; (set-input-mode (car (current-input-mode)) (nth 1 (current-input-mode)) 0)
-;; (autoload 'ispell-word "ispell" "Check the spelling of word in buffer." 't)
-;; (autoload 'ispell-region "ispell" "Check the spelling of region." 't)
-;; (autoload 'ispell-buffer "ispell" "Check the spelling of buffer." t)
-;; (setq ispell-dictionary "francais"
-;;      ispell-skip-sgml t)
-;; (autoload 'flyspell-mode "flyspell" "On-the-fly spelling checking" t)
-;; (setq flyspell-default-dictionary "french")
-
-;; ; highlight region between point and mark
-;; (transient-mark-mode t)
-
-;; (setq kill-whole-line t)
-;; (setq mouse-yank-at-point t)
-
-; Make all "yes or no" prompts show "y or n" instead
-;; (fset 'yes-or-no-p 'y-or-n-p)
-
-
-; Start with new default.
-;; (setq mode-line-format default-mode-line-format)
-
-;; (setq line-number-mode t)
-;; (setq column-number-mode t)
+(setq kill-whole-line t)
+(setq mouse-yank-at-point t)
 
 (provide 'starter-kit-misc-olivier)
